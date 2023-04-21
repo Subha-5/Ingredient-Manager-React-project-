bb55a197a0496172f8cb7f076224cb3f24019c7a
Subha-5 <subhasadhu5@gmail.com>
2023-04-22 00:44:43 +0530

    useEffect hook

--- src/components/Ingredients/Ingredients.jsx
+++ src/components/Ingredients/Ingredients.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,6 +7,27 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
 
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://learn-react-hooks-b2651-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setIngredients(loadedIngredients);
+      })
+  }, [])
+
+  useEffect( () => {
+    console.log('RENDERING INGREDIENTS', ingredients)
+  }, [ingredients])
+
 
   const addIngredientHandler = (ingredient) => {
     fetch('https://learn-react-hooks-b2651-default-rtdb.firebaseio.com/ingredients.json', {
@@ -18,7 +39,7 @@ const Ingredients = () => {
     }).then(responseData => {
       setIngredients(prevIngredients =>
         [...prevIngredients,
-        { id: Math.random().toString(), ...ingredient }
+        { id: responseData.name, ...ingredient }
         ])
     })
   }
@@ -27,12 +48,16 @@ const Ingredients = () => {
     setIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingredientID))
   }
 
+  const filteredIngredientsHandler = (filteredIngredients) => {
+    setIngredients(filteredIngredients);
+  }
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>

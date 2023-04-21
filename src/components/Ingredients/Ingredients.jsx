25f36966b3016c9d3a0f81a01d458743acda01bc
Subha-5 <subhasadhu5@gmail.com>
2023-04-21 16:13:54 +0530

    useState Hooks

---
+++ src/components/Ingredients/Ingredients.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import IngredientForm from './IngredientForm';
+import Search from './Search';
+import IngredientList from './IngredientList';
+
+const Ingredients = () => {
+
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredients(prevIngredients => {return [...prevIngredients, {id: Math.random().toString(), ...ingredient}]});
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
